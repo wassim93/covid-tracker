587f0eb4e2c14e7a550a7e906776add8149bff81
wassim93 <wassim.benhassen@esprit.tn>
2022-01-30 23:51:31 +0100

    adding map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./map.css";
 const Map = () => {
-  return <div className="map">Map here</div>;
+  const center = { lat: 34.80746, lng: -40.4796 };
+  return (
+    <div className="map">
+      <MapContainer zoom="3" center={center}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
